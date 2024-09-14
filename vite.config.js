bebd1bfbeb9d482ab5d67036b0c12bcfad67db84
Gurkirat Singh <94279121+gurkiratz@users.noreply.github.com>
2024-09-14 17:52:55 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ios-world-clock',
+  base: '/ios-world-clock/',
   plugins: [react()],
 })
